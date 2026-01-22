8829df41ca0ba962d5194995b77ed7d3ce0ac8db
crimson-future <amoody648@gmail.com>
2026-01-21 20:21:40 -0500

    Initial shard_edit method work done. Preliminary bug test done.

--- tracker_v1.1.py
+++ tracker_v1.1.py
@@ -113,6 +113,111 @@ class Character:
         print(f"{self.player_name.capitalize()}'s corruption points decreased by {abs(rec_decr)}. The new points for the player is {self.current_points}. Their tier is {self.tier_level}.\n")
         savestate(players, filename)
 
+    
+    def edit_shards(self): # 8
+        while True:
+            print(f"Current shard list for {self.player_name}: {self.shard_colors}\n")
+            choice = input("Welcome to the shard editor. What would you like to do? Enter a number to select:\n0: Go back without changing anything\n1: Add a shard to the character\n2: Remove a shard from the character\n3: Change the character's shard\n\n Selection: ")
+            if choice == "0": 
+                clear_screen()
+                print("No further changes made. Returning to menu.")
+                time.sleep(1.0)
+                break
+            else: 
+                while True:
+                    clear_screen()
+                    print(f"Current shard list: {self.shard_colors}\n")
+                    #Add to list
+                    if choice == "1": 
+                        clear_screen()
+                        color=input("Please enter a shard color to add, or type 0 to quit. Example: Red\n")
+                        while True:
+                            clear_screen()
+                            if color=="0":
+                                clear_screen()  
+                                print("No further changes made. Returning to menu.")
+                                break
+                            else:
+                                color=color.capitalize()
+                                self.shard_colors.append(color)
+                                clear_screen()
+                                print(f"\n Shard added.\n")
+                                savestate(players, filename)
+
+                                break
+                        break
+                    #Remove from list
+                    elif choice == "2":
+                        clear_screen()
+                        print("Pick a shard the player will remove by number, or type 0 to go back: ") #0 is set as Exit option to avoid complications around the incremented choice numbers
+                        while True:
+                            # If there are no shards left, exit
+                            if not self.shard_colors:
+                                print("No shards, returning...")
+                                time.sleep(1.0)
+                                clear_screen()
+                                print("No shards left to remove.\n")
+                                break
+                            print("0: Return to shard menu")
+                            for i, shard in enumerate(self.shard_colors, start=1):
+                                print(f"{i}: {shard}")
+                            shard_choice = input("\nSelection: ")
+                            if shard_choice == "0":
+                                time.sleep(1.0)
+                                clear_screen() 
+                                print("No further changes made. Returning to menu.")
+                                break
+                            try:
+                                index = int(shard_choice) - 1
+                                if index < 0 or index >= len(self.shard_colors):
+                                    print("That shard number doesn't exist. Please try again.")
+                                    continue
+                                print(f"Removing shard {self.shard_colors[index]} from the list for self...")
+                                time.sleep(1.0)
+                                del self.shard_colors[index]
+                                savestate(players, filename)
+                                break
+                            except ValueError:
+                                print("Bad input (not a number). Please try again.")
+                        break
+                    #Change a list value
+                    elif choice == "3":
+                        clear_screen()
+                        print("Pick a shard the player will modify by number, or type 0 to go back: ") #0 is set as Exit option to avoid complications around the incremented choice numbers
+                        while True:
+                            # If there are no shards left, exit
+                            if not self.shard_colors:
+                                time.sleep(1.0)
+                                clear_screen()
+                                print("No shards to edit.")
+                                break
+                            print("0: Return to shard menu")
+                            for i, shard in enumerate(self.shard_colors, start=1):
+                                print(f"{i}: {shard}")
+                            shard_choice = input("\nSelection: ")
+                            if shard_choice == "0":
+                                time.sleep(1.0)
+                                clear_screen()
+                                print("No further changes made. Returning to menu.")
+                                break
+                            try:
+                                index = int(shard_choice) - 1
+                                if index < 0 or index >= len(self.shard_colors):
+                                    print("That shard number doesn't exist. Please try again.")
+                                    continue
+                                selection = input("What would you like to replace this shard value with?\n")
+                                print("Replacing shard...")
+                                time.sleep(1.0)
+                                self.shard_colors[index] = selection
+                                clear_screen()
+                                print("Shard changed successfully!")  
+                                savestate(players, filename)
+                                break
+                            except ValueError:
+                                print("Bad input (not a number). Please try again.")
+                            
+                        break             
+
     def reset_char(self): #11
         while True:
             choice=input(f"Are you sure you want to reset all values for {self.player_name}? y/n: ")
@@ -132,6 +237,7 @@ class Character:
             else:
                 print("Bad input. Please try again. ")
                 continue
+
 
 #6 is currently Character Select
 #7 is currently Show All Characters
@@ -219,7 +325,7 @@ def menu_framework(players):
             "act_name": "Show All Stat Blocks" 
         },{
             "act_num" : 8, 
-            "act_name": "placeholder - Select Active Shard"
+            "act_name": "Edit Player Shards"
         },{
             "act_num" : 9, 
             "act_name": "placeholder - Change Day Count"
@@ -340,11 +446,15 @@ def menu_framework(players):
             clear_screen()
             return action_num 
 
+        elif action_num == 8: #Edit the shards of the selected player (likely not gonna get much use)
+            print(f"Selected: {action_name}\n")
+            if selected_char != None:
+                selected_char.edit_shards()
+            else:
+                print("No character selected! No changes made.\n")
+            return action_num
+
 #Not Yet Implemented - Placeholders
-        elif action_num == 8:
-            print(action_name)
-            print("Placeholder, feature not yet implemented.")
-            return action_num  
         elif action_num == 9:
             print(action_name)
             print("Placeholder, feature not yet implemented.")
